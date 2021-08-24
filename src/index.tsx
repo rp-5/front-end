cb5ef0bf51b3024aa160f1761a5b2c7b7fb3129b
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-08-24 11:33:55 -0300

    create-react-app

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './Routes';
+import './index.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Routes></Routes>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
