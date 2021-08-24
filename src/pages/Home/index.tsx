6a7df6b1e5817febc456d9891aced639f6798b92
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-08-24 12:10:13 -0300

    home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,16 @@
+import Icon from '@ailibs/feather-react-ts';
 import React from 'react';
 import styles from './Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.cardWrapper}>
+      <button className={styles.cardItem}>
+        <div>
+          <Icon name="file-plus" />
+          <p>Adicionar amostra</p>
+        </div>
+      </button>
     </div>
   );
 }
