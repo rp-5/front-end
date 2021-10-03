b20208f051bb4604d4489cdfe30871b3cdadc626
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-10-03 17:56:15 -0300

    intensity

--- src/pages/Amostra/new.tsx
+++ src/pages/Amostra/new.tsx
@@ -458,7 +458,6 @@ export const NewAmostra = () => {
                           label="Intensidade"
                         >
                           <MenuItem value={'N/A'}>Não se aplica</MenuItem>
-                          <MenuItem value={'0'}>0</MenuItem>
                           <MenuItem value={'1'}>1</MenuItem>
                           <MenuItem value={'2'}>2</MenuItem>
                           <MenuItem value={'3'}>3</MenuItem>
