3ac0d595d26405d7fc6b4cc371ed0e80a64107f5
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-10-03 19:49:45 -0300

    refactor formik fields

--- src/pages/Amostra/formik.tsx
+++ src/pages/Amostra/formik.tsx
@@ -1,30 +1,133 @@
-export const initialValues: { [key: string]: string } = {
-  dogName: '',
-  dogRace: '',
-  dogGender: '',
-  dogAge: '',
-  dogIsVaccinated: '',
-  dogUsesCollar: '',
-  dogColor: '',
-  dogHair: '',
-  dogEnvironment: '',
-  dogHasContacts: '',
-  ownerName: '',
-  ownerPhone: '',
-  locationStreet: '',
-  locationComplement: '',
-  locationNeighborhood: '',
-  locationArea: '',
-  locationLatitude: '',
-  locationLongitude: '',
-  sampleN: '',
-  sampleNumber: '',
-  sampleDate: '',
-  sampleLVC: '',
-  sampleIsDead: '',
-  sintName: '',
-  sintIntensity: '',
-  exameName: '',
-  exameDate: '',
-  exameResult: '',
+interface AmostraObject {
+  amostra: Amostra
+  sintomas: Sintoma[]
+  acao: Acao
+  exames: Exame[]
+  proprietario: Proprietario
+}
+
+interface Amostra {
+  numero?: number
+  lvc: boolean
+  morreu: boolean
+}
+
+interface Sintoma {
+  nome: string
+  intensidade?: number
+}
+
+interface Acao {
+  nome: string
+}
+
+interface Exame {
+  data: string
+  nome: string
+  resultado?: number
+}
+
+interface Proprietario {
+  nome: string
+  caes: Cao[]
+  localizacoes: Localizacao[]
+}
+
+interface Cao {
+  nome: string
+  raca: string
+  sexo: string
+  idade: number
+  vacina: boolean
+  usaColeira: boolean
+  cor: string
+  pelo: string
+  ambiente: string
+  temContato: boolean
+}
+
+interface Localizacao {
+  endereco: string
+  complemento?: string
+  bairro: string
+  area: string
+  latitude: number
+  longitude: number
+}
+
+export const initialValues: AmostraObject = {
+  amostra: {
+    numero: undefined,
+    lvc: false,
+    morreu: false
+  },
+  sintomas: [
+    {
+      nome: '',
+      intensidade: undefined,
+    },
+  ],
+  acao: {
+    nome: ''
+  },
+  exames: [
+    {
+      nome: '',
+      data: '',
+      resultado: undefined,
+    }
+  ],
+  proprietario: {
+    nome: '',
+    caes: [
+      {
+        nome: '',
+        idade: 0,
+        raca: '',
+        sexo: '',
+        usaColeira: false,
+        vacina: false,
+        cor: '',
+      }
+    ],
+    localizacoes: [
+      {
+        area: '',
+        bairro: '',
+        endereco: '',
+        latitude: 0,
+        longitude: 0,
+        complemento: '',
+      }
+    ]
+  },
+  // dogName: '',
+  // dogRace: '',
+  // dogGender: '',
+  // dogAge: '',
+  // dogIsVaccinated: '',
+  // dogUsesCollar: '',
+  // dogColor: '',
+  // dogHair: '',
+  // dogEnvironment: '',
+  // dogHasContacts: '',
+  // ownerName: '',
+  // ownerPhone: '',
+  // locations: [],
+  // locationStreet: '',
+  // locationComplement: '',
+  // locationNeighborhood: '',
+  // locationArea: '',
+  // locationLatitude: '',
+  // locationLongitude: '',
+  // sampleN: '',
+  // sampleNumber: '',
+  // sampleDate: '',
+  // sampleLVC: '',
+  // sampleIsDead: '',
+  // sintName: '',
+  // sintIntensity: '',
+  // exameName: '',
+  // exameDate: '',
+  // exameResult: '',
 }
